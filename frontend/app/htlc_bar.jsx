d878cf6213513e31f3c24afea23bcb7a1a422b04
Conner Babinchak <connerbabs@gmail.com>
2023-11-18 00:34:10 -0500

    Add basic HTLC fee percentage visualization

---
+++ frontend/app/htlc_bar.jsx
@@ -0,0 +1,32 @@
+import React, { useRef, useEffect } from 'react';
+import * as d3 from 'd3';
+
+const HorizontalBar = ({ leftPercentage }) => {
+    const ref = useRef();
+
+    useEffect(() => {
+        const svg = d3.select(ref.current);
+        const width = 500;
+        const height = 50;
+
+        const leftWidth = width * leftPercentage / 100;
+        const rightWidth = width * (100 - leftPercentage) / 100;
+
+        svg.selectAll('rect').remove();
+
+        svg.append('rect')
+            .attr('width', leftWidth)
+            .attr('height', height)
+            .attr('fill', 'blue');
+
+        svg.append('rect')
+            .attr('x', leftWidth)
+            .attr('width', rightWidth)
+            .attr('height', height)
+            .attr('fill', 'green');
+    }, [leftPercentage]);
+
+    return <svg ref={ref} width="500" height="50" />;
+};
+
+export default HorizontalBar;
