6b6e53ed71026ce75148f39790941881fa64df59
Conner Babinchak <connerbabs@gmail.com>
2023-11-18 16:26:53 -0500

    Rename setPercentage

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -4,7 +4,7 @@ import HorizontalBar from './htlc_bar'
 import { useEffect, useState } from 'react'
 
 export default function Home() {
-  const [percentage, usePercentage] = useState(30)
+  const [percentage, setPercentage] = useState(30)
   const [blockFeeRate, setBlockFeeRate] = useState('128')
   const [htlcAmount, setHtlcAmount] = useState('55000')
 
@@ -13,14 +13,14 @@ export default function Home() {
     const blockFee = parseInt(blockFeeRate) * vbytesPerHtlc
     const percentFee = blockFee * 100 / parseInt(htlcAmount)
     console.log('percentFee', percentFee, blockFee, parseInt(htlcAmount))
-    usePercentage(percentFee)
+    setPercentage(percentFee)
 
   }, [blockFeeRate, htlcAmount])
 
   return (
     <>
       {/* <div>
-        <input type="range" min="0" max="100" value={percentage} onChange={(e) => usePercentage(parseInt(e.target.value))} />
+        <input type="range" min="0" max="100" value={percentage} onChange={(e) => setPercentage(parseInt(e.target.value))} />
       </div> */}
 
       <div>
